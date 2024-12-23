3ddf64a2c240b50f73920acf4972695893726358
Kiên <110784389+Kn45nb@users.noreply.github.com>
2024-12-23 15:07:57 +0700

    Update Check_Input.ino

--- Check_Input/Check_Input.ino
+++ Check_Input/Check_Input.ino
@@ -1,10 +1,8 @@
 #include <Wire.h>
 
 void setup() {
-  Serial.begin(115200);
-  Wire.setSDA(1); // GPIO1 for SDA
-  Wire.setSCL(2); // GPIO2 for SCL
-  Wire.begin();
+  Serial.begin(9600);
+  Wire.begin(); // Use default SDA (GPIO4) and SCL (GPIO5)
 
   Serial.println("Scanning I2C devices...");
   for (uint8_t address = 1; address < 127; address++) {
