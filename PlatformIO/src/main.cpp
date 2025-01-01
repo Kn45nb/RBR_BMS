333c3958bba41d92112ad880b1fd3a7f79ce5afe
Kiên <knight.45.acgg@gmail.com>
2025-01-01 15:57:23 +0700

    main.cpp

--- PlatformIO/src/main.cpp
+++ PlatformIO/src/main.cpp
@@ -1,18 +1,19 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Chân đèn LED tích hợp trên Pico (GPIO 25)
+#define LED_PIN 25
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // Thiết lập chân LED là OUTPUT
+  pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  // Bật LED
+  digitalWrite(LED_PIN, HIGH);
+  delay(500);  // Chờ 500ms
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // Tắt LED
+  digitalWrite(LED_PIN, LOW);
+  delay(500);  // Chờ 500ms
 }
